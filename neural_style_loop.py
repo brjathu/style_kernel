343b3b3d606b279787edd6773cc1cdcde8f42c95
brjathu <brjathu@gmail.com>
2017-09-10 21:09:39 +1000

    first commit

---
+++ neural_style_loop.py
@@ -0,0 +1,228 @@
+# Copyright (c) 2015-2017 Anish Athalye. Released under GPLv3.
+
+import os
+
+import numpy as np
+import scipy.misc
+
+from stylize import stylize
+
+import math
+from argparse import ArgumentParser
+
+from PIL import Image
+
+# default arguments
+CONTENT_WEIGHT = 5e0
+CONTENT_WEIGHT_BLEND = 1
+STYLE_WEIGHT = 5e2  # 5e2
+TV_WEIGHT = 1e2
+STYLE_LAYER_WEIGHT_EXP = 1
+LEARNING_RATE = 1e1
+BETA1 = 0.9
+BETA2 = 0.999
+EPSILON = 1e-08
+STYLE_SCALE = 1.0
+ITERATIONS = 1000
+VGG_PATH = 'imagenet-vgg-verydeep-197.mat'
+POOLING = 'max'
+
+
+def build_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--content',
+                        dest='content', help='content image',
+                        metavar='CONTENT')
+    parser.add_argument('--styles',
+                        dest='styles',
+                        nargs='+', help='one or more style images',
+                        metavar='STYLE')
+    parser.add_argument('--output',
+                        dest='output', help='output path',
+                        metavar='OUTPUT')
+    parser.add_argument('--iterations', type=int,
+                        dest='iterations', help='iterations (default %(default)s)',
+                        metavar='ITERATIONS', default=ITERATIONS)
+    parser.add_argument('--print-iterations', type=int,
+                        dest='print_iterations', help='statistics printing frequency',
+                        metavar='PRINT_ITERATIONS')
+    parser.add_argument('--checkpoint-output',
+                        dest='checkpoint_output', help='checkpoint output format, e.g. output%%s.jpg',
+                        metavar='OUTPUT')
+    parser.add_argument('--checkpoint-iterations', type=int,
+                        dest='checkpoint_iterations', help='checkpoint frequency',
+                        metavar='CHECKPOINT_ITERATIONS')
+    parser.add_argument('--width', type=int,
+                        dest='width', help='output width',
+                        metavar='WIDTH')
+    parser.add_argument('--style-scales', type=float,
+                        dest='style_scales',
+                        nargs='+', help='one or more style scales',
+                        metavar='STYLE_SCALE')
+    parser.add_argument('--network',
+                        dest='network', help='path to network parameters (default %(default)s)',
+                        metavar='VGG_PATH', default=VGG_PATH)
+    parser.add_argument('--content-weight-blend', type=float,
+                        dest='content_weight_blend', help='content weight blend, conv4_2 * blend + conv5_2 * (1-blend) (default %(default)s)',
+                        metavar='CONTENT_WEIGHT_BLEND', default=CONTENT_WEIGHT_BLEND)
+    parser.add_argument('--content-weight', type=float,
+                        dest='content_weight', help='content weight (default %(default)s)',
+                        metavar='CONTENT_WEIGHT', default=CONTENT_WEIGHT)
+    parser.add_argument('--style-weight', type=float,
+                        dest='style_weight', help='style weight (default %(default)s)',
+                        metavar='STYLE_WEIGHT', default=STYLE_WEIGHT)
+    parser.add_argument('--style-layer-weight-exp', type=float,
+                        dest='style_layer_weight_exp', help='style layer weight exponentional increase - weight(layer<n+1>) = weight_exp*weight(layer<n>) (default %(default)s)',
+                        metavar='STYLE_LAYER_WEIGHT_EXP', default=STYLE_LAYER_WEIGHT_EXP)
+    parser.add_argument('--style-blend-weights', type=float,
+                        dest='style_blend_weights', help='style blending weights',
+                        nargs='+', metavar='STYLE_BLEND_WEIGHT')
+    parser.add_argument('--tv-weight', type=float,
+                        dest='tv_weight', help='total variation regularization weight (default %(default)s)',
+                        metavar='TV_WEIGHT', default=TV_WEIGHT)
+    parser.add_argument('--learning-rate', type=float,
+                        dest='learning_rate', help='learning rate (default %(default)s)',
+                        metavar='LEARNING_RATE', default=LEARNING_RATE)
+    parser.add_argument('--beta1', type=float,
+                        dest='beta1', help='Adam: beta1 parameter (default %(default)s)',
+                        metavar='BETA1', default=BETA1)
+    parser.add_argument('--beta2', type=float,
+                        dest='beta2', help='Adam: beta2 parameter (default %(default)s)',
+                        metavar='BETA2', default=BETA2)
+    parser.add_argument('--eps', type=float,
+                        dest='epsilon', help='Adam: epsilon parameter (default %(default)s)',
+                        metavar='EPSILON', default=EPSILON)
+    parser.add_argument('--initial',
+                        dest='initial', help='initial image',
+                        metavar='INITIAL')
+    parser.add_argument('--initial-noiseblend', type=float,
+                        dest='initial_noiseblend', help='ratio of blending initial image with normalized noise (if no initial image specified, content image is used) (default %(default)s)',
+                        metavar='INITIAL_NOISEBLEND')
+    parser.add_argument('--preserve-colors', action='store_true',
+                        dest='preserve_colors', help='style-only transfer (preserving colors) - if color transfer is not needed')
+    parser.add_argument('--pooling',
+                        dest='pooling', help='pooling layer configuration: max or avg (default %(default)s)',
+                        metavar='POOLING', default=POOLING)
+    return parser
+
+
+def main():
+    parser = build_parser()
+    options = parser.parse_args()
+
+    if not os.path.isfile(options.network):
+        parser.error("Network %s does not exist. (Did you forget to download it?)" % options.network)
+
+    content_images = sorted(os.listdir("examples/content"))
+    style_images = sorted(os.listdir("examples/style"))
+    range_sigma = [1e18, 1e19]  # , 1e21, 1e22, 1e25, 1e30]
+    range_sw = [1e10, 1e15]  # , 1e20, 1e25, 1e30, 1e40, 1e50]
+
+    count = 0
+    for c in content_images:
+        for s in style_images:
+            for sig in range_sigma:
+                for sw in range_sw:
+                    c_image = imread("examples/content/" + c)
+                    s_image = [imread("examples/style/" + s)]
+
+                    width = options.width
+                    if width is not None:
+                        new_shape = (int(math.floor(float(c_image.shape[0]) /
+                                                    c_image.shape[1] * width)), width)
+                        c_image = scipy.misc.imresize(c_image, new_shape)
+                    target_shape = c_image.shape
+                    for i in range(len(s_image)):
+                        style_scale = STYLE_SCALE
+                        if options.style_scales is not None:
+                            style_scale = options.style_scales[i]
+                        s_image[i] = scipy.misc.imresize(s_image[i], style_scale *
+                                                         target_shape[1] / s_image[i].shape[1])
+
+                    style_blend_weights = options.style_blend_weights
+                    if style_blend_weights is None:
+                        # default is equal weights
+                        style_blend_weights = [1.0 / 1]
+                    else:
+                        total_blend_weight = sum(style_blend_weights)
+                        style_blend_weights = [weight / total_blend_weight
+                                               for weight in style_blend_weights]
+
+                    initial = options.initial
+                    if initial is not None:
+                        initial = scipy.misc.imresize(imread(initial), c_image.shape[:2])
+                        # Initial guess is specified, but not noiseblend - no noise should be blended
+                        if options.initial_noiseblend is None:
+                            options.initial_noiseblend = 0.0
+                    else:
+                        # Neither inital, nor noiseblend is provided, falling back to random generated initial guess
+                        if options.initial_noiseblend is None:
+                            options.initial_noiseblend = 1.0
+                        if options.initial_noiseblend < 1.0:
+                            initial = c_image
+
+                    if options.checkpoint_output and "%s" not in options.checkpoint_output:
+                        parser.error("To save intermediate images, the checkpoint output "
+                                     "parameter must contain `%s` (e.g. `foo%s.jpg`)")
+
+                    count += 1
+                    sname = c[0:-4] + "_" + s[0:-4] + "_" + str(sw) + "_" + str(sig) + ".jpg"
+                    print("loop ==> " + str(count) + "of" + str(len(content_images) * len(style_images) * len(range_sigma) * len(range_sw)))
+
+                    print("--content " + c + " --styles " + s + " --output final/exp/" + sname + " --iterations 1000 --style-weight " + str(sw))
+
+                    text_file = open("Output.txt", "a")
+                    text_file.write("image name: " + sname + '\n')
+                    text_file.close()
+                    for iteration, image in stylize(
+                        network=options.network,
+                        initial=initial,
+                        initial_noiseblend=options.initial_noiseblend,
+                        content=c_image,
+                        styles=s_image,
+                        preserve_colors=options.preserve_colors,
+                        iterations=10,
+                        content_weight=options.content_weight,
+                        content_weight_blend=options.content_weight_blend,
+                        style_weight=sw,
+                        style_layer_weight_exp=options.style_layer_weight_exp,
+                        style_blend_weights=style_blend_weights,
+                        tv_weight=options.tv_weight,
+                        learning_rate=options.learning_rate,
+                        beta1=options.beta1,
+                        beta2=options.beta2,
+                        epsilon=options.epsilon,
+                        pooling=options.pooling,
+                        print_iterations=options.print_iterations,
+                        checkpoint_iterations=options.checkpoint_iterations,
+                        exp_sigma=sig
+                    ):
+                        # output_file = None
+                        combined_rgb = image
+                        # if iteration is not None:
+                        #     if options.checkpoint_output:
+                        #         output_file = options.checkpoint_output % iteration
+                        # else:
+                        #     output_file = options.output
+                        # if output_file:
+                        imsave("final/exp/" + str(sig) + "/" + sname, combined_rgb)
+
+
+def imread(path):
+    img = scipy.misc.imread(path).astype(np.float)
+    if len(img.shape) == 2:
+        # grayscale
+        img = np.dstack((img, img, img))
+    elif img.shape[2] == 4:
+        # PNG with alpha channel
+        img = img[:, :, :3]
+    return img
+
+
+def imsave(path, img):
+    img = np.clip(img, 0, 255).astype(np.uint8)
+    Image.fromarray(img).save(path, quality=95)
+
+
+if __name__ == '__main__':
+    main()
---
+++ stylize.py
@@ -0,0 +1,331 @@
+# Copyright (c) 2015-2017 Anish Athalye. Released under GPLv3.
+
+import vgg
+
+import tensorflow as tf
+import numpy as np
+
+from sys import stderr
+import matplotlib.pylab as plt
+from PIL import Image
+
+CONTENT_LAYERS = ('relu4_2', 'relu5_2')
+STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1', 'relu5_1')
+
+#exponential kernal
+# sig = 30000
+
+
+#mattern kernal
+rho = 10000
+sigMat = 1e4
+v=2.5
+
+
+try:
+    reduce
+except NameError:
+    from functools import reduce
+
+
+def stylize(network, initial, initial_noiseblend, content, styles, preserve_colors, iterations,
+        content_weight, content_weight_blend, style_weight, style_layer_weight_exp, style_blend_weights, tv_weight,
+        learning_rate, beta1, beta2, epsilon, pooling,
+        print_iterations=None, checkpoint_iterations=None , kernel=0, exp_sigma=20000):
+
+    
+
+    tf.logging.set_verbosity(tf.logging.INFO)
+    """
+    Stylize images.
+
+    This function yields tuples (iteration, image); `iteration` is None
+    if this is the final image (the last iteration).  Other tuples are yielded
+    every `checkpoint_iterations` iterations.
+
+    :rtype: iterator[tuple[int|None,image]]
+
+    0 - dot product kernel 
+    1 - exponential kernel
+    2 - matern kernel
+    3 - polynomial kernel
+
+    """
+    shape = (1,) + content.shape
+    style_shapes = [(1,) + style.shape for style in styles]
+    content_features = {}
+    style_features = [{} for _ in styles]
+
+    vgg_weights, vgg_mean_pixel = vgg.load_net(network)
+
+    layer_weight = 1.0
+    style_layers_weights = {}
+    for style_layer in STYLE_LAYERS:
+        style_layers_weights[style_layer] = layer_weight
+        layer_weight *= style_layer_weight_exp
+
+    # normalize style layer weights
+    layer_weights_sum = 0
+    for style_layer in STYLE_LAYERS:
+        layer_weights_sum += style_layers_weights[style_layer]
+    for style_layer in STYLE_LAYERS:
+        style_layers_weights[style_layer] /= layer_weights_sum
+
+    # compute content features in feedforward mode
+    g = tf.Graph()
+    with g.as_default(), g.device('/cpu:0'), tf.Session() as sess:
+        image = tf.placeholder('float', shape=shape)
+        net = vgg.net_preloaded(vgg_weights, image, pooling)
+        content_pre = np.array([vgg.preprocess(content, vgg_mean_pixel)])
+        for layer in CONTENT_LAYERS:
+            content_features[layer] = net[layer].eval(feed_dict={image: content_pre})
+
+    # compute style features in feedforward mode
+    for i in range(len(styles)):
+        g = tf.Graph()
+        with g.as_default(), g.device('/cpu:0'), tf.Session() as sess:
+            image = tf.placeholder('float', shape=style_shapes[i])
+            net = vgg.net_preloaded(vgg_weights, image, pooling)
+            style_pre = np.array([vgg.preprocess(styles[i], vgg_mean_pixel)])
+            for layer in STYLE_LAYERS:
+                features = net[layer].eval(feed_dict={image: style_pre})
+                features = np.reshape(features, (-1, features.shape[3]))
+                                
+                # sqr = features.T*features.T
+                # dim = features.shape
+
+               
+                if(kernel==0):
+                    gram2 =   np.matmul(features.T, features)/ features.size
+                elif(kernel==1):
+                    gram2 = gramExp_np(features, exp_sigma)/ features.size    #exponential kernal
+                elif(kernel==2):
+                    gram2 = gramMatten_np(features, sigMat , v , rho) / features.size     #Mattern kernal
+                elif(kernel==3):
+                    gram2 = gramPoly_np(features, 2) / features.size
+
+
+
+
+                print(features.shape,"diamention of feature\n")
+
+
+                style_features[i][layer] = gram2
+
+    initial_content_noise_coeff = 1.0 - initial_noiseblend
+
+    # make stylized image using backpropogation
+    g = tf.Graph()
+    with g.as_default(), g.device('/gpu:0'):
+        if initial is None:
+            noise = np.random.normal(size=shape, scale=np.std(content) * 0.1)
+            initial = tf.random_normal(shape) * 0.256
+        else:
+            initial = np.array([vgg.preprocess(initial, vgg_mean_pixel)])
+            initial = initial.astype('float32')
+            noise = np.random.normal(size=shape, scale=np.std(content) * 0.1)
+            initial = (initial) * initial_content_noise_coeff + (tf.random_normal(shape) * 0.256) * (1.0 - initial_content_noise_coeff)
+        image = tf.Variable(initial)
+        net = vgg.net_preloaded(vgg_weights, image, pooling)
+
+        # content loss
+        content_layers_weights = {}
+        content_layers_weights['relu4_2'] = content_weight_blend
+        content_layers_weights['relu5_2'] = 1.0 - content_weight_blend
+
+        content_loss = 0
+        content_losses = []
+        for content_layer in CONTENT_LAYERS:
+            content_losses.append(content_layers_weights[content_layer] * content_weight * (2 * tf.nn.l2_loss(
+                    net[content_layer] - content_features[content_layer]) /
+                    content_features[content_layer].size))
+        content_loss += reduce(tf.add, content_losses)
+
+        # style loss
+        style_loss = 0
+        for i in range(len(styles)):
+            style_losses = []
+            for style_layer in STYLE_LAYERS:
+                layer = net[style_layer]
+                _, height, width, number = map(lambda i: i.value, layer.get_shape())
+                size = height * width * number
+                feats = tf.reshape(layer, (-1, number))                
+
+                style_gram = style_features[i][style_layer]
+
+                dim = feats.get_shape()
+                print(dim)
+
+                sqr = tf.reduce_sum(tf.transpose(feats)*tf.transpose(feats),axis=1)
+
+                if(kernel==0):
+                    gram =(tf.matmul(tf.transpose(feats), feats) )/ size
+                elif(kernel==1):
+                    gram = tf.exp(-1* (tf.transpose( tf.ones([dim[1],dim[1]])* sqr) + tf.ones([dim[1],dim[1]])* sqr - 2*tf.matmul(tf.transpose(feats), feats) ) /2/(exp_sigma*exp_sigma))/size  #exponetial kernal
+                elif(kernel==2):
+                    #mattern kernal
+                    d2 =    tf.nn.relu(tf.transpose( tf.ones([dim[1],dim[1]])* sqr) + tf.ones([dim[1],dim[1]])* sqr - 2*tf.matmul(tf.transpose(feats), feats) );
+                    if(v==2.5):
+                        gram =   sigMat**2 * (   tf.ones([dim[1],dim[1]]) + tf.sqrt(5.0) * tf.sqrt(d2) / rho   + 5 *  d2 / 3 / (rho**2) )   * tf.exp(-1 * tf.sqrt(5.0)  *  tf.sqrt(d2) / rho  ) /size 
+                        #gram = tf.Print(gram, [gram], message="This is gram: ")
+ 
+                style_losses.append(style_layers_weights[style_layer] * 2 * tf.nn.l2_loss(gram - style_gram) / style_gram.size)
+
+            style_loss += style_weight * style_blend_weights[i] * reduce(tf.add, style_losses)
+
+        # total variation denoising
+        tv_y_size = _tensor_size(image[:,1:,:,:])
+        tv_x_size = _tensor_size(image[:,:,1:,:])
+        
+        tv_loss = tv_weight * 2 * (
+                (tf.nn.l2_loss(image[:,1:,:,:] - image[:,:shape[1]-1,:,:]) /
+                    tv_y_size) +
+                (tf.nn.l2_loss(image[:,:,1:,:] - image[:,:,:shape[2]-1,:]) /
+                    tv_x_size))
+
+
+
+        aaa = image[:,1:,:,:] - image[:,:shape[1]-1,:,:]
+        bbb = tf.nn.l2_loss(image[:,:,1:,:] - image[:,:,:shape[2]-1,:])
+        ccc = (tf.nn.l2_loss(image[:,1:,:,:] - image[:,:shape[1]-1,:,:]) / tv_y_size)
+        ddd = (tf.nn.l2_loss(image[:,:,1:,:] - image[:,:,:shape[2]-1,:]) / tv_x_size)
+
+        
+        
+        # overall loss
+        loss = content_loss + style_loss + tv_loss
+        
+        # optimizer setup
+        # train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+        train_step = tf.train.AdamOptimizer(learning_rate, beta1, beta2, epsilon).minimize(loss)
+
+        def print_progress():
+            stderr.write('  content loss: %g\n' % content_loss.eval())
+            stderr.write('    style loss: %g\n' % style_loss.eval())
+            stderr.write('       tv loss: %g\n' % tv_loss.eval())
+            stderr.write('    total loss: %g\n' % loss.eval())
+
+
+            text_file = open("Output.txt", "a")
+            text_file.write('  content loss: %g\n' % content_loss.eval())
+            text_file.write('    style loss: %g\n' % style_loss.eval())
+            text_file.write('       tv loss: %g\n' % tv_loss.eval())
+            text_file.write('    total loss: %g\n\n\n' % loss.eval())
+            text_file.close()
+
+        # optimization
+        best_loss = float('inf')
+        best = None
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            stderr.write('Optimization started...\n')
+            if (print_iterations and print_iterations != 0):
+                print_progress()
+            for i in range(iterations):
+                stderr.write('Iteration %4d/%4d\n' % (i + 1, iterations))
+                # print(sess.run(aaa),sess.run(bbb),sess.run(ccc),sess.run(ddd))
+                # print(sess.run(d2))
+                # print(sess.run(gram))
+                train_step.run()
+
+                last_step = (i == iterations - 1)
+                if last_step or (print_iterations and i % print_iterations == 0):
+                    print_progress()
+
+                if (checkpoint_iterations and i % checkpoint_iterations == 0) or last_step:
+                    this_loss = loss.eval()
+                    print(this_loss,"loss in each check point")
+                    if this_loss < best_loss:
+                        best_loss = this_loss
+                        best = image.eval()
+
+                    img_out = vgg.unprocess(best.reshape(shape[1:]), vgg_mean_pixel)
+
+                    if preserve_colors and preserve_colors == True:
+                        original_image = np.clip(content, 0, 255)
+                        styled_image = np.clip(img_out, 0, 255)
+                        
+
+                        # Luminosity transfer steps:
+                        # 1. Convert stylized RGB->grayscale accoriding to Rec.601 luma (0.299, 0.587, 0.114)
+                        # 2. Convert stylized grayscale into YUV (YCbCr)
+                        # 3. Convert original image into YUV (YCbCr)
+                        # 4. Recombine (stylizedYUV.Y, originalYUV.U, originalYUV.V)
+                        # 5. Convert recombined image from YUV back to RGB
+
+                        # 1
+                        styled_grayscale = rgb2gray(styled_image)
+                        styled_grayscale_rgb = gray2rgb(styled_grayscale)
+
+                        # 2
+                        styled_grayscale_yuv = np.array(Image.fromarray(styled_grayscale_rgb.astype(np.uint8)).convert('YCbCr'))
+
+                        # 3
+                        original_yuv = np.array(Image.fromarray(original_image.astype(np.uint8)).convert('YCbCr'))
+
+                        # 4
+                        w, h, _ = original_image.shape
+                        combined_yuv = np.empty((w, h, 3), dtype=np.uint8)
+                        combined_yuv[..., 0] = styled_grayscale_yuv[..., 0]
+                        combined_yuv[..., 1] = original_yuv[..., 1]
+                        combined_yuv[..., 2] = original_yuv[..., 2]
+
+                        # 5
+                        img_out = np.array(Image.fromarray(combined_yuv, 'YCbCr').convert('RGB'))
+
+
+                    yield (
+                        (None if last_step else i),
+                        img_out
+                    )
+
+
+def _tensor_size(tensor):
+    from operator import mul
+    return reduce(mul, (d.value for d in tensor.get_shape()), 1)
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+
+def gray2rgb(gray):
+    w, h = gray.shape
+    rgb = np.empty((w, h, 3), dtype=np.float32)
+    rgb[:, :, 2] = rgb[:, :, 1] = rgb[:, :, 0] = gray
+    return rgb
+
+
+
+
+
+
+
+
+
+
+def gramExp_np(features,sigma):
+    #exponential kernal
+    sqr = features.T*features.T
+    dim = features.shape
+    return np.exp(-1*( (np.ones((dim[1],dim[1]), dtype=np.int)*np.sum(sqr,axis=1)).T + (np.ones((dim[1],dim[1]), dtype=np.int)*np.sum(sqr,axis=1)) - 2*(np.matmul(features.T,features)))/2/sigma/sigma)
+
+def gramPoly_np(features,sigma, C=0):
+    #Polynomial kernal
+    return (np.matmul(features.T, features) + C)**d
+
+def gramMatten_np(features,sigma, v , rho):
+    #matttern kernel
+    sqr = features.T*features.T
+    dim = features.shape
+    d2 = abs( (np.ones((dim[1],dim[1]), dtype=np.int)*np.sum(sqr,axis=1)).T + (np.ones((dim[1],dim[1]), dtype=np.int)*np.sum(sqr,axis=1)) - 2*(np.matmul(features.T,features)))
+    print(d2.shape)
+    if(v==0.5):
+        return sigma**2 * np.exp(-1*np.sqrt(d2) / rho)
+    if(v==1.5):
+        return sigma**2 * (np.ones((dim[1],dim[1]),dtype=np.int) + np.sqrt(3) * np.sqrt(d2) / rho ) * np.exp(-1*np.sqrt(3) * np.sqrt(d2) / rho)
+    if(v==2.5):
+        return sigma**2 * (np.ones((dim[1],dim[1]),dtype=np.int) + np.sqrt(5) * np.sqrt(d2) / rho  +  5 * d2 / 3 / rho**2 ) * np.exp(-1*np.sqrt(5) * np.sqrt(d2) / rho)
+
+
+
+#polynomial kernel
